b069e9014d2c0aa5de84ae5a3f464de88a2046d7
Skyblade <Skybladev2@users.noreply.github.com>
2018-01-05 23:47:11 +0300

    introduce keyboard controls

--- Assets/SphereController.cs
+++ Assets/SphereController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -25,31 +26,27 @@ public class SphereController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Escape))
-        {
-            Application.Quit();
-        }
-
-        var mouseDelta = Input.mousePosition - _mousePos;
-
-        var horizontalDelta = mouseDelta.x;
-
-        var rot = Quaternion.Euler(0, horizontalDelta, 0) * _playerToCameraVector;
-        
-        Camera.transform.SetPositionAndRotation(this.transform.position + rot, Quaternion.identity);
-        Camera.transform.LookAt(transform.position);
-
+        CheckForExit();
+        RotateCamera();
         var cameraToPlayer = (this.transform.position - Camera.transform.position).normalized;
+        ProcessInput(cameraToPlayer);
 
-        if (Input.GetMouseButton(0))
-        {
-            _rigidbody.AddTorque(cameraToPlayer.z * Torque, 0, -cameraToPlayer.x * Torque);
-        }
+        //Debug.Log(_rigidbody.velocity);
+    }
 
-        if (Input.GetMouseButton(1))
-        {
-            _rigidbody.AddTorque(-cameraToPlayer.z * Torque, 0, cameraToPlayer.x * Torque);
-        }
+    private void ProcessInput(Vector3 cameraToPlayer)
+    {
+        ProcessKeyboardInput(cameraToPlayer);
+    }
+
+    private void ProcessKeyboardInput(Vector3 cameraToPlayer)
+    {
+        var vertical = Input.GetAxisRaw("Vertical");
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        _rigidbody.AddTorque(
+            (cameraToPlayer.z * vertical - cameraToPlayer.x * horizontal) * Torque, 
+            0, 
+            (-cameraToPlayer.x * vertical  - cameraToPlayer.z * horizontal) * Torque);
 
         if (Input.GetButton("Jump"))
         {
@@ -59,7 +56,36 @@ public class SphereController : MonoBehaviour
         {
             _rigidbody.angularDrag = _initialAngularDrag;
         }
-        
-        Debug.Log(_rigidbody.velocity);
+    }
+
+    //private void ProcessMouseInput(Vector3 cameraToPlayer)
+    //{
+    //    if (Input.GetMouseButton(0))
+    //    {
+    //        _rigidbody.AddTorque(cameraToPlayer.z * Torque, 0, -cameraToPlayer.x * Torque);
+    //    }
+
+    //    if (Input.GetMouseButton(1))
+    //    {
+    //        _rigidbody.AddTorque(-cameraToPlayer.z * Torque, 0, cameraToPlayer.x * Torque);
+    //    }
+    //}
+
+    private void RotateCamera()
+    {
+        var mouseDelta = Input.mousePosition - _mousePos;
+        var horizontalDelta = mouseDelta.x;
+        var rot = Quaternion.Euler(0, horizontalDelta, 0) * _playerToCameraVector;
+
+        Camera.transform.SetPositionAndRotation(this.transform.position + rot, Quaternion.identity);
+        Camera.transform.LookAt(transform.position);
+    }
+
+    private static void CheckForExit()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
 }
